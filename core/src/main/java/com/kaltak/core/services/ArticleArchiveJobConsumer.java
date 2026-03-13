2ab2457ccc78a76d4d51a9fcdb7712bba63ab209
arpita <arpita.rani@axeno.co>
2026-03-13 17:20:34 +0530

    sling job

--- core/src/main/java/com/kaltak/core/services/ArticleArchiveJobConsumer.java
+++ core/src/main/java/com/kaltak/core/services/ArticleArchiveJobConsumer.java
@@ -1,22 +1,35 @@
-package com.kaltak.core.schedulers;
+package com.kaltak.core.services;
 
 import java.util.*;
+
 import javax.jcr.Session;
+
 import org.apache.sling.api.resource.*;
+import org.apache.sling.event.jobs.Job;
+import org.apache.sling.event.jobs.consumer.JobConsumer;
 import org.osgi.service.component.annotations.*;
 import org.osgi.service.metatype.annotations.Designate;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.day.cq.search.*;
 import com.day.cq.search.result.*;
 
 import com.kaltak.core.config.kaltak_archiver_config;
 
-@Component(service = {Runnable.class, kaltak_archive_scheduler.class}, immediate = true)
+@Component(
+        service = JobConsumer.class,
+        property = {
+                JobConsumer.PROPERTY_TOPICS + "=kaltak/archive/articles"
+        }
+)
 @Designate(ocd = kaltak_archiver_config.class)
-public class kaltak_archive_scheduler implements Runnable {
+public class ArticleArchiveJobConsumer implements JobConsumer {
 
-    private static final Logger LOG = LoggerFactory.getLogger(kaltak_archive_scheduler.class);
+    private static final Logger LOG =
+            LoggerFactory.getLogger(ArticleArchiveJobConsumer.class);
+
     private static final String SUBSERVICE = "content-writer";
 
     @Reference
@@ -30,14 +43,14 @@ public class kaltak_archive_scheduler implements Runnable {
     @Activate
     @Modified
     protected void activate(kaltak_archiver_config config) {
+
         this.config = config;
-        LOG.info("Archiver Scheduler activated with cron: {}",
-                config.scheduler_expression());
     }
 
     @Override
-    public void run() {
-        LOG.info("Starting Archiver...");
+    public JobResult process(Job job) {
+
+        LOG.info("Starting Sling Job Archiver...");
 
         int successCount = 0;
         int failureCount = 0;
@@ -45,63 +58,82 @@ public class kaltak_archive_scheduler implements Runnable {
         Map<String, Object> serviceMap = new HashMap<>();
         serviceMap.put(ResourceResolverFactory.SUBSERVICE, SUBSERVICE);
 
-        try (ResourceResolver resolver = resolverFactory.getServiceResourceResolver(serviceMap)) {
+        try (ResourceResolver resolver =
+                     resolverFactory.getServiceResourceResolver(serviceMap)) {
+
             Session session = resolver.adaptTo(Session.class);
+
             List<String> oldArticles = findOldArticles(session);
-            LOG.info("Number of articles that can be archived: {}", oldArticles.size());
+
+            LOG.info("Articles eligible for archiving: {}", oldArticles.size());
 
             int batchSize = config.batch_size();
 
             for (int i = 0; i < oldArticles.size(); i += batchSize) {
+
                 int endIndex = Math.min(i + batchSize, oldArticles.size());
+
                 List<String> batch = oldArticles.subList(i, endIndex);
 
                 try {
+
                     for (String articlePath : batch) {
+
                         String articleName =
                                 articlePath.substring(articlePath.lastIndexOf("/") + 1);
+
                         String destination =
                                 config.archive_root() + "/" + articleName;
 
-                        LOG.info("articles archiving {}", articlePath);
+                        LOG.info("Archiving article {}", articlePath);
 
                         session.move(articlePath, destination);
                     }
 
                     session.save();
+
                     successCount += batch.size();
 
                 } catch (Exception e) {
 
-                    LOG.error("Failing batch. Reverting batch starting with {}",
+                    LOG.error("Batch failed. Reverting batch starting with {}",
                             batch.get(0), e);
 
                     session.refresh(false);
+
                     failureCount += batch.size();
                 }
             }
 
         } catch (Exception e) {
 
-            LOG.error("Error in scheduler", e);
+            LOG.error("Error executing archive job", e);
+
+            return JobResult.FAILED;
         }
 
-        LOG.info("So articles archived -> Success: {}, Failed: {}",
+        LOG.info("Archiving completed -> Success: {}, Failed: {}",
                 successCount, failureCount);
+
+        return JobResult.OK;
     }
 
     private List<String> findOldArticles(Session session) {
 
         List<String> results = new ArrayList<>();
+
         Calendar limitDate = Calendar.getInstance();
+
         limitDate.add(Calendar.DAY_OF_YEAR, -config.days_limit());
 
         Map<String, String> map = new HashMap<>();
 
         map.put("path", config.news_root());
         map.put("type", "cq:Page");
+
         map.put("1_property", "jcr:content/cq:lastReplicated");
         map.put("1_property.operation", "exists");
+
         map.put("2_daterange.property", "jcr:content/cq:lastReplicated");
         map.put("2_daterange.upperBound", limitDate.toInstant().toString());
         map.put("2_daterange.upperOperation", "<");
@@ -110,15 +142,18 @@ public class kaltak_archive_scheduler implements Runnable {
 
         Query query =
                 queryBuilder.createQuery(PredicateGroup.create(map), session);
+
         SearchResult result = query.getResult();
 
         try {
 
             for (Hit hit : result.getHits()) {
+
                 results.add(hit.getPath());
             }
 
         } catch (Exception e) {
+
             LOG.error("Error retrieving old articles", e);
         }
 
---
+++ core/src/main/java/com/kaltak/core/servlets/ArticleArchiveServlet.java
@@ -0,0 +1,36 @@
+package com.kaltak.core.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.*;
+
+import org.osgi.service.component.annotations.*;
+
+import com.kaltak.core.services.ArticleArchiveJobProducer;
+
+@Component(
+        service = Servlet.class,
+        property = {
+                "sling.servlet.paths=/bin/start-article-archive",
+                "sling.servlet.methods=" + HttpConstants.METHOD_GET
+        }
+)
+public class ArticleArchiveServlet extends SlingAllMethodsServlet {
+
+    @Reference
+    private ArticleArchiveJobProducer producer;
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request,
+                         SlingHttpServletResponse response)
+            throws IOException {
+
+        producer.startArchiveJob();
+
+        response.getWriter().write("Archive Sling Job triggered");
+    }
+}
